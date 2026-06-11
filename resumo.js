57b3c92ea27d6f111110c234b980d245c74306de
Maria Clara ESquerdo Cunha <14165995681@estudante.ufjf.br>
2026-06-11 14:31:02 -0300

    :tada: add arquivo resumo e inicia ele

---
+++ resumo.js
@@ -0,0 +1,4 @@
+// Escopo => local onde ficam variáveis, constantes e funções
+// Existem dois tipos de escopo: global e local
+// O escopo global é acessível em todo o código
+// O escopo local é acessível apenas dentro de uma função ou bloco
